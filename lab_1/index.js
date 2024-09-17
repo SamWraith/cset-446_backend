c09f920624ff7a46f14b3d6329ec989a89477baa
SamWraith <samirnandan2003@gmail.com>
2024-09-17 13:50:15 +0530

    Refactor docker-compose.yaml to add MongoDB service with authentication and volume configuration

--- lab_1/index.js
+++ lab_1/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
+const { default: connectDB } = require('./db/connect');
+dotenv.config({
+    path: "./.env"
+});
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World!!!</h1>");
 });
-
+connectDB();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
